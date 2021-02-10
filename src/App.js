13e50e067c6d4bc3883114039f61afcd0401128d
Ketan Shukla <ketankshukla@gmail.com>
2021-02-10 00:54:21 -0800

    completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from "react";
-import "./styles.css";
-// comment
-const App = () => {
-  return <div className="App">Vanilla React App!</div>;
-};
+import React from 'react'
+import Navbar from './components/Navbar'
+import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Reports from './pages/Reports'
+import Products from './pages/Products'
 
-export default App;
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/reports' component={Reports} />
+          <Route path='/products' component={Products} />
+        </Switch>
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { SidebarData } from './SidebarData'
+import { IconContext } from 'react-icons'
+import * as AiIcons from 'react-icons/ai'
+import * as FaIcons from 'react-icons/fa'
+import './Navbar.css'
+
+const Navbar = () => {
+  const [sidebar, setSideBar] = useState(false)
+  const showSidebar = () => setSideBar(!sidebar)
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: 'yellow' }}>
+        <div className='navbar'>
+          <Link to='#' className='menu-bars'>
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+        </div>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className='nav-click-items' onClick={showSidebar}>
+            <li className='navbar-toggle'>
+              <Link to='#' className='menu-bars'>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
+  )
+}
+
+export default Navbar
